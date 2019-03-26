ad786effff2acc1780208b375dd8a9722ed43794
iamPavan17 <iampavan05@gmail.com>
2019-03-26 13:27:01 +0530

    first commit

---
+++ Assignment/hca.js
@@ -0,0 +1,20 @@
+var a = [
+    {
+        doctor: 'Rama',
+        expertise : [ 'asthma', 'eczema', 'food allergies'],
+        specialty: 'Allergists'
+
+    },
+    {
+        doctor: 'Sita',
+        expertise : [ 'heart failure', 'heart attack', 'high blood pressure'],
+        specialty: 'Cardiologists'
+    }
+];
+
+var b = 'i have high blood pressure';
+var c = b.split(' ');
+console.log(c);
+a.forEach(function(a) {
+    
+});
