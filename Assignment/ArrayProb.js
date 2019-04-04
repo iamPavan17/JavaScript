36693a6570d980736967ce572ae8a0c8d9af22ad
iamPavan17 <iampavan05@gmail.com>
2019-04-04 15:36:01 +0530

    hacker rank Array Problem

---
+++ Assignment/ArrayProb.js
@@ -0,0 +1,16 @@
+function ArrayProb(arr, k) {
+    let ans = 0;
+    for(var i = 0; i < k; i++) {
+        let maxValue = Math.max.apply(Math, arr);
+        ans = ans + maxValue;
+        let indexValue = arr.indexOf(maxValue);
+        arr.splice(indexValue, 1);
+        let divi = Math.floor(maxValue) / 2;
+        arr.splice(indexValue, 0, divi);
+    }
+    return ans;
+}
+
+var arr = [1,2,3,4,5];
+var k = 2;
+console.log(ArrayProb(arr, k));
