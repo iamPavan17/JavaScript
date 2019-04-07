ea4be492abe1f00bb6af508ad85967ba7b5ea5bb
iamPavan17 <iampavan05@gmail.com>
2019-04-07 15:57:54 +0530

    iife

--- hca1/message-api.js
+++ hca1/message-api.js
@@ -1,34 +1,26 @@
-function message(phone, messBody) {
-    var unirest = require("unirest");
 
-var req = unirest("GET", "https://www.fast2sms.com/dev/bulk");
+(function () {
+  var unirest = require("unirest");
 
-req.query({
-  "authorization": "j4qN5V68iwZp7ScGLEyoUFduJ3kCtTrQAhIMsx1l2afWnOKeYD5ciXK1swQSL2WvkYaCquRA8gd0ZIoH",
-  "sender_id": "FSTSMS",
-  "message": `
-    <h2>${messBody.name}</h2>
-    <h3>${messBody.text}</h3>
-  `,
-  "language": "english",
-  "route": "p",
-  "numbers": phone,
-});
+  var req = unirest("GET", "https://www.fast2sms.com/dev/bulk");
+  
+  req.query({
+    "authorization": "j4qN5V68iwZp7ScGLEyoUFduJ3kCtTrQAhIMsx1l2afWnOKeYD5ciXK1swQSL2WvkYaCquRA8gd0ZIoH",
+    "sender_id": "FSTSMS",
+    "message": "Test",
+    "language": "english",
+    "route": "p",
+    "numbers": 8553562168,
+  });
+  
+  req.headers({
+    "cache-control": "no-cache"
+  });
+  
 
-req.headers({
-  "cache-control": "no-cache"
-});
-
-
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
-
-  console.log(res.body);
-});
-}
-
-messBody = {
-    name: 'Pavan',
-    text: 'Test message'
-}
-message(8553562168, messBody);
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+  
+    console.log(res.body);
+  });
+})();
